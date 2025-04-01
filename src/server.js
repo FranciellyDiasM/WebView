4b1d88047b4a6e97035c216816c6fc32eba9d839
FranciellyDiasM <frandiasma@gmail.com>
2025-04-01 16:07:51 -0300

    test

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const { createServer } = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end(`
+      <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Meu site</title>
+        </head>
+        <body>
+            <h1>ESTE É O MEU SITE COM MEU SERVIDOR</h1>
+        </body>
+      </html>
+  `);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
